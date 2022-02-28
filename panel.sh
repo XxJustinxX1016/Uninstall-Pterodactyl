fcb29e9692716eee06f8ef99df68def5742ee6ff
XxJustinxX1016 <98905045+XxJustinxX1016@users.noreply.github.com>
2022-02-28 18:29:55 +0700

    Create panel.sh

---
+++ panel.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+########################################################
+# 
+#          Uninstall Panel
+#
+#         
+#
+#            Thank You
+#
+########################################################
+
+
+SCRIPT_VERSION="v1.0"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+#### Colors ####
+
+reset="\e[0m"
+red='\033[0;31m'
+
+error() {
+  red='\033[0;31m'
+  reset="\e[0m"
+
+  echo ""
+  echo -e "* ${red}ERROR${reset}: $1"
+  echo ""
+}
+
+
+#### Check Sudo ####
+
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+
+#### Check Curl ####
+
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+cancel() {
+echo
+echo -e "* ${red}Installation Canceled!${reset}"
+done=true
+exit 1
+}
+
+done=false
+
+
+sudo rm -rf /var/www/pterodactyl
+
+sudo rm /etc/systemd/system/pteroq.service
+
+sudo unlink /etc/nginx/sites-enabled/pterodactyl.conf
+
+done
